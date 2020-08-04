522bf3bbeaaefa38cc6904f98ebe4048f36e88ec
Kyle Stough <kstough00@gmail.com>
2020-08-04 17:07:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,6 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese()
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
